7bf9e746de70f83a945c6e40838d1bec4b34cba2
starikna <don.strekoza@gmail.com>
2023-02-24 14:04:41 +0700

    First commit

--- src/js/__tests__/testFunction.js
+++ src/js/__tests__/testFunction.js
@@ -1,18 +1,96 @@
-import healthSort from '../app';
+import Character from '../app';
 
-test('test sortHealth', () => {
-  const input = [
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ];
+describe('test class Character', () => {
+  class Zombie extends Character {
+    constructor(name, type = 'Zombie', attack = 10, defence = 40) {
+      super(name, type, attack, defence);
+    }
+  }
 
-  const expected = [
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ];
+  // class Bowman extends Character {
+  //   constructor(name, type = 'Bowman', attack = 25, defence = 25) {
+  //     super(name, type, attack, defence);
+  //   }
+  // }
 
-  const received = healthSort(input);
-  expect(received).toEqual(expected);
+  // class Swordsman extends Character {
+  //   constructor(name, type = 'Swordsman', attack = 40, defence = 10) {
+  //     super(name, type, attack, defence);
+  //   }
+  // }
+
+  // class Magician extends Character {
+  //   constructor(name, type = 'Magician', attack = 10, defence = 40) {
+  //     super(name, type, attack, defence);
+  //   }
+  // }
+
+  // class Undead extends Character {
+  //   constructor(name, type = 'Undead', attack = 25, defence = 25) {
+  //     super(name, type, attack, defence);
+  //   }
+  // }
+
+  // class Daemon extends Character {
+  //   constructor(name, type = 'Daemon', attack = 40, defence = 10) {
+  //     super(name, type, attack, defence);
+  //   }
+  // }
+
+  test('Class is defined', () => {
+    expect(Character).toBeDefined();
+  });
+
+  test('test check name length > 1', () => {
+    const received = new Zombie('Nat');
+    const expected = 1;
+    expect(received.name.length).toBeGreaterThan(expected);
+  });
+
+  test('test check name length < 11', () => {
+    const received = new Zombie('Nat');
+    const expected = 11;
+    expect(received.name.length).toBeLessThan(expected);
+  });
+
+  test('test check name = "string"', () => {
+    const received = new Zombie('Nat');
+    const expected = 'string';
+    expect(typeof received.name).toBe(expected);
+  });
+
+  test('test function levelUp', () => {
+    const preReceived = new Zombie('Cat');
+    preReceived.levelUp();
+    const expected = 2;
+    expect(preReceived.level).toBe(expected);
+  });
+
+  test('test function damage', () => {
+    const received = new Zombie('Mat');
+    received.damage(20);
+    const expected = 88;
+    expect(received.health).toBe(expected);
+  });
+
+  test('test function checkName', () => {
+    const preReceived = new Zombie('P');
+    const received = preReceived.checkName();
+    const expected = 'Ошибка длины имени!';
+    expect(received).toThrow(expected);
+  });
+
+  test('test function checkNameLength', () => {
+    const preReceived = new Zombie('Pittttttttttttttttt');
+    const received = preReceived.checkNameLength();
+    const expected = 'Ошибка длины имени!';
+    expect(received).toThrow(expected);
+  });
+
+  test('test function checkNameType', () => {
+    const preReceived = new Zombie('12');
+    const received = preReceived.checkNameType();
+    const expected = 'Имя - не строка!';
+    expect(received).toThrow(expected);
+  });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,39 @@
-export default function healthSort(arrayWarriors = []) {
-  arrayWarriors.sort((prev, next) => next.health - prev.health);
-  return arrayWarriors;
+export default class Character {
+  constructor(name, type, attack, defence, health = 100, level = 1) {
+    this.name = name;
+    this.type = type;
+    this.attack = attack;
+    this.defence = defence;
+    this.health = health;
+    this.level = level;
+
+    this.checkNameLength = function () {
+      if (this.name.length < 2 || this.name.length > 10) {
+        throw new Error('Ошибка длины имени!');
+      }
+    };
+
+    this.checkNameType = function () {
+      if (typeof this.name !== 'string') {
+        throw new Error('Имя - не строка!');
+      }
+    };
+
+    this.levelUp = function () {
+      if (this.health) {
+        this.level += 1;
+
+        this.attack += (this.attack * 20) / 100;
+        this.defence += (this.defence * 20) / 100;
+
+        this.health = 100;
+      }
+    };
+
+    this.damage = function (points) {
+      if (this.health >= 0) {
+        this.health -= points * (1 - this.defence / 100);
+      }
+    };
+  }
 }
-
-// const arrayOfWarriors = [
-//   {name: 'мечник', health: 10},
-//   {name: 'маг', health: 100},
-//   {name: 'лучник', health: 80},
-// ]
-
-// healthSort(arrayOfWarriors);
